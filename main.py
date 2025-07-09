6ae5571eddad7f81ff227919ad17e7d850339f57
Tejas-kasmo <tejas.natraj@kasmodigital.com>
2025-07-09 10:58:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if 'Contents' in responses:
     for obj in responses['Contents']:
         file_key = obj['Key']
         
-        name, email, phone, skills, linkedin = transform_resume.get_structured_data(file_key=file_key, s3=s3)
-        load_to_sql_server.connect(name, email, phone, skills, linkedin)
+        name, email, phone, skills, linkedin, github = transform_resume.get_structured_data(file_key=file_key, s3=s3)
+        load_to_sql_server.connect(name, email, phone, skills, linkedin, github)
         
